43084996765cbb0b803cbbf09ba5b14bba275d2d
matheuscassemiro1 <127998481+matheuscassemiro1@users.noreply.github.com>
2024-09-30 12:24:19 -0300

    feat: add cron-job 11,17h to notify users

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { JsonDB, Config } from 'node-json-db'
 import cron from 'node-cron';
-import { createServer } from "http"
-import cors from 'cors'
 
-
-
-import { apiRouter } from './routes/api.js';
 import { prefeituraController } from './controller/prefeituraController.js';
 
-import { carregarComandosTelegram } from './botTelegram.js';
-
-
-
-//SETUP DO SERVIDOR
-export const app = express();
-export const httpListener = createServer(app);
-
-
-//RECEPÇÃO DE DADOS E CABEÇALHO
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true,
-}));
-app.disable('X-Powered-By');
-
-///CORS --> TEMPORÁRIO
-app.use(cors({
-    origin: '*',
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'Cookie'],
-    methods: "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-}));
-app.use('', apiRouter);
+import { carregarComandosTelegram, dispararNotificacaoUsuariosAtivos } from './botTelegram.js';
 
 /// AGENDAMENTO DISPARA A CADA 6H
-const tarefa = cron.schedule('* */6 * * *', async () => {
-    prefeituraController.buscarAtualizacao();
-    console.log("cronjob executado");
-    console.log(new Date().toTimeString());
-}, { scheduled: false });
+const tarefa = cron.schedule('0 11,17 * * *', async () => {
+    console.log("Início da Tarefa Agendada")
+    const hora = new Date().toTimeString()
+    console.log(hora);
+    await dispararNotificacaoUsuariosAtivos(hora);
+    //prefeituraController.buscarAtualizacao();
+    console.log("Fim da Tarefa Agendada");
+});
 
 
-try{
+try {
     console.log("Menu de comandos inseridos no Telegram");
     await carregarComandosTelegram();
 } catch (error) {
